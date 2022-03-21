98fd9fe26c32f112fcf68a026b09f3da9cfe4cad
Pedro <pedro.novaes.education@gmail.com>
2022-03-21 10:13:52 -0300

    footer updated

--- public/scripts/scrollPokedexBehaviour.js
+++ public/scripts/scrollPokedexBehaviour.js
@@ -3,7 +3,7 @@ let indexLog = []
 var pokemons = [];
 
 const fetchPokemons = async() => {
-    const queryOffset = document.querySelectorAll(".pokedex__card").length + 1;
+    const queryOffset = document.querySelectorAll(".pokemon__card").length + 1;
 
     for (let i = queryOffset; i <= queryOffset + 19; i++) {
         await getPokemons(i, pokemons);       
@@ -44,7 +44,7 @@ function createCard(pokemon){
     const pokemonTypes = pokemon.types;
 
     const card = document.createElement("div");
-    card.classList.add("pokedex__card", "rounded", "bg__darkgray", "fluid");
+    card.classList.add("pokemon__card", "rounded", "bg__darkgray", "fluid");
     
     const cardIndex = document.createElement("div");
     cardIndex.classList.add("pokemon__index", "text__color__lightblack", "bg__gray");
@@ -60,23 +60,23 @@ function createCard(pokemon){
 
 
     const cardCover = document.createElement("div");
-    cardCover.classList.add("pokedex__card__cover", "fluid", "bg__lightblack");
+    cardCover.classList.add("pokemon__card__cover", "fluid", "bg__lightblack");
     cardCover.style.backgroundImage = `url(${image})`;
     
     const cardInfo = document.createElement("div");
-    cardInfo.classList.add("pokedex__card__info");
+    cardInfo.classList.add("pokemon__card__info");
 
     const pokemonName = document.createElement("div");
-    pokemonName.classList.add("pokedex__card__info__name", "text__color__gray");
+    pokemonName.classList.add("pokemon__card__info__name", "text__color__gray");
     pokemonName.innerText = pokemon.name;
     cardInfo.appendChild(pokemonName);
 
     const typeContainer = document.createElement("div");
-    typeContainer.classList.add("pokedex__type__container");
+    typeContainer.classList.add("pokemon__type__container");
 
     pokemonTypes.forEach(pokemonType => {
         const typeElement = document.createElement("div");
-        typeElement.classList.add("pokedex__type", "rounded", `${pokemonType.type.name}`, "bg__lightblack");
+        typeElement.classList.add("pokemon__type", "rounded", `${pokemonType.type.name}`, "bg__lightblack");
 
         const typeName = document.createElement("p");
         typeName.classList.add("text__family__sigmar");
@@ -91,5 +91,5 @@ function createCard(pokemon){
         card.appendChild(element);
     });
 
-    document.querySelector("#pokedex__grid").appendChild(card);
+    document.querySelector("#pokemon__grid").appendChild(card);
 }
